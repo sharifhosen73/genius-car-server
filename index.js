38bc2be1281d45093ffd0a333dd01dc67b9b0dbf
Sharif <sharifhosen73@gmail.com>
2022-11-03 16:07:14 +0600

    patch added

--- index.js
+++ index.js
@@ -54,6 +54,26 @@ async function run() {
       const result = await orderCollection.insertOne(order);
       res.send(result);
     });
+
+    app.patch("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await orderCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
